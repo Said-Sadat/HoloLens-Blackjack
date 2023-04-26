a907f889eff0341379f4606c4974ca0aa0be401d
Said Mozamil Sadat <mozzy.sadat@gmail.com>
2023-04-26 19:59:21 +0100

    Random Audio around player

---
+++ Assets/Scripts/RandomReactionAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomReactionAudio : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] List<AudioSource> sources;
+
+    float maxtime = 3f;
+    float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxtime)
+        {
+            PlayRandomAudio();
+            timer = 0f;
+        }
+    }
+
+    public void PlayRandomAudio()
+    {
+        int randomIndex = Random.Range(0, audioClips.Count);
+        int randomSource = Random.Range(0, sources.Count);
+
+        sources[randomSource].PlayOneShot(audioClips[randomIndex]);
+    }
+}
